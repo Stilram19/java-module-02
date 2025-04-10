ce2227ee9fd6c2b561c571c019fb24b604b5544d
Omar Bednaoui <obednaou@amazon.com>
2025-04-10 09:54:32 +0100

    test

--- ex00/Program.java
+++ ex00/Program.java
@@ -9,6 +9,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+// github test
+
 class Program {
     public static void main(String[] args) {
         Path workDir = Paths.get(System.getProperty("user.dir"));
